2a4e5533655de94278e264b5711fa5de92fc4e3a
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-18 13:10:51 +0530

    Create MainABC.java

---
+++ HierachicalInheritance_parameter13.02/MainABC.java
@@ -0,0 +1,15 @@
+class MainABC{
+	public static void main(String args[]){
+		B objb=new B(15,17);
+		C objc=new C(33,87);
+		
+		objb.gety();
+		objc.getz();
+		objb.getx();
+		//objc.getx();
+	}
+}
+	
+	
+	
+		
