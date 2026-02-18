4a774d9c61ab0b2a7fdb7b601fc8b7dc9893a854
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-18 14:04:46 +0530

    Create Student5.java

---
+++ java26.01.28/Student5.java
@@ -0,0 +1,14 @@
+class Student5{
+	int id;
+	String name;
+	
+	Student5(){
+		this.id=1003;
+		this.name="Vignarathan";
+	}
+	
+	void getstudent(){
+		System.out.println("Student ID : "+this.id);
+		System.out.println("Student Name : "+this.name);
+	}
+}
