abf1674a75dfbaaa2e56181c2136b71ea62f8500
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-18 14:04:49 +0530

    Create Student6.java

---
+++ java26.01.28/Student6.java
@@ -0,0 +1,14 @@
+class Student6{
+	int id;
+	String name;
+	
+	Student6(int id,String name){
+		this.id=id;
+		this.name=name;
+	}
+	
+	void getstudent(){
+		System.out.println("Student ID : "+this.id);
+		System.out.println("Student Name : "+this.name);
+	}
+}
