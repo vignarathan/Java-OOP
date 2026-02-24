cb431aa96a4365a8ed84b8c6a68de487aab8188d
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-24 11:13:38 +0530

    Add Mainstudent.java to demonstrate overloading
    Add a new Mainstudent.java with a main method that creates five Student instances and invokes overloaded setstudent methods (no-arg, int, String, int+String, String+int) to demonstrate method overloading. The Student class implementation is expected elsewhere.

---
+++ overloading_23.02.2026/Mainstudent.java
@@ -0,0 +1,17 @@
+//Overloading
+class Mainstudent{
+	public static void main(String args[]){
+		Student st1=new Student();
+		Student st2=new Student();
+		Student st3=new Student();
+		Student st4=new Student();
+		Student st5=new Student(); 
+		
+		st1.setstudent();
+		st2.setstudent(4007);
+		st3.setstudent("Vikki");
+		st4.setstudent(5005,"v.rathan");
+		st5.setstudent("vigna",2026);
+	}
+}
+		
