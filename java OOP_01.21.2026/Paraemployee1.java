f2a8a279f477a726c0bbd1236a47613b7fe22a79
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-10 16:56:25 +0530

    Create Paraemployee1.java

---
+++ java OOP_01.21.2026/Paraemployee1.java
@@ -0,0 +1,27 @@
+class Paraemployee1{
+	int id;
+	String name;
+	double basic;
+	double bonus;
+	
+	void calcbonus(){
+		if(basic>=100000){
+			this.bonus=basic*0.05;
+		}else{
+			this.bonus=basic*0.03;
+		}
+	}
+	void setemployee(int id,String name,double basic){
+		this.id=id;
+		this.name=name;
+		this.basic=basic;
+	}
+	void display(){
+		System.out.println("Employee ID : "+this.id);
+		System.out.println("Employee Name : "+this.name);
+		System.out.println("Baisc Salary : "+this.basic);
+		System.out.println("Bonus emount : "+this.bonus);
+		System.out.println("Net Salary : "+(this.basic + this.bonus));
+	}
+}
+		
