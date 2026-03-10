26433196c48ecfac3e2d1506ea7f3b6530d35377
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-10 16:56:34 +0530

    Create Paramainemployee1.java

---
+++ java OOP_01.21.2026/Paramainemployee1.java
@@ -0,0 +1,12 @@
+class Paramainemployee1{
+	public static void main(String args[]){
+		Paraemployee1 em=new Paraemployee1();
+		
+		em.setemployee(1005,"Vignarathan",1000000);
+		em.calcbonus();
+		em.display();
+	}
+}
+		
+		
+		
