21446975e79a5cf8a7fd5e42b5b8563b10d85781
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-10 16:56:19 +0530

    Create Paraadmission.java

---
+++ java OOP_01.21.2026/Paraadmission.java
@@ -0,0 +1,15 @@
+class Paraadmission{
+	int id;
+	String name;
+	
+	void setparaadmission(int id,String name){
+		this.id=id;
+		this.name=name;
+	}
+	
+	void getparaadmission(){
+		System.out.println("Student ID : "+this.id);
+		System.out.println("Student Name : "+this.name);
+	}
+}
+		
