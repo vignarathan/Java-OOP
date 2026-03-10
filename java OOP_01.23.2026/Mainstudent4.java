d43249b3bb77f10f28f7235494fa5f4758e6dcb1
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-10 16:56:43 +0530

    Create Mainstudent4.java

---
+++ java OOP_01.23.2026/Mainstudent4.java
@@ -0,0 +1,15 @@
+class Mainstudent4{
+	public static void main(String args[]){
+		
+		Student4 st=new Student4();
+		
+		st.setdata(1003,"Vignarathan","Sivalingam");
+		st.getfullname();
+		st.display();
+	}
+}
+		
+			
+			
+			
+		
