16721f0ebc35a79eea233247d56e54c665a9e076
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-26 09:25:19 +0530

    Create C.java

---
+++ Interface_25.03.2026/multilevel_interface/C.java
@@ -0,0 +1,16 @@
+class C implements IB{
+	int z;
+	C(){
+		this.z=666;
+	}
+	public void getx(){
+		System.out.println("IAX is "+X);
+	}
+	public void gety(){
+		System.out.println("IBY is "+Y);
+	}
+	void getz(){
+		System.out.println("Cz is "+this.z);
+	}
+	
+}
