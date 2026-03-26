1bdc0e8c9f0b616ee39352d07c98a8546e652f25
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-26 09:24:57 +0530

    Update A.java

--- final nonaccess modifier_24.03.2026/A.java
+++ final nonaccess modifier_24.03.2026/A.java
@@ -1,7 +1,7 @@
 class A{
 	static final int X=25;
 	A(){
-		//this.x=20;
+		//this.x=20; final varaible or static final variable should be initilaize while declaration
 	}
 	static final void getx(){
 		System.out.println("Ax "+this.x);
