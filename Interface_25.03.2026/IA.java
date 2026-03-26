ad9a9e89cb9aa9fecea844254991176cf3e3caad
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-26 09:25:10 +0530

    Create IA.java

---
+++ Interface_25.03.2026/IA.java
@@ -0,0 +1,8 @@
+interface IA{
+	int X=10;
+	
+	void getx();
+}
+//interface hasn't constructor
+//all methods in interface are public abstract methods
+//variables in interface are public static final variables
